7f5c4b4caf206e94cb754fcbdc4455ac81f7660f
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-10-20 14:56:03 +0200

    Quick fix: disable debug (#6)

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-let DEBUG = true;
+let DEBUG = false;
 let API_BASE_URL = DEBUG ? 'http://localhost:5000' : 'https://ghosterysearch.com';
 let SERP_BASE_URL = DEBUG ? 'http://localhost' : 'https://ghosterysearch.com';
