5bf4cfd06efcf6e69b242a31936f1674d77f6c4f
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-10-23 16:19:03 +0200

    Choice Screen second step

--- src/choice-screen.js
+++ src/choice-screen.js
@@ -1,6 +1,6 @@
 async function onboarding() {
   const { isOnboardingCompleted } = await browser.storage.local.get("isOnboardingCompleted");
-  if (!isOnboardingCompleted) {
+  if (DEBUG || !isOnboardingCompleted) {
     await browser.tabs.create({
       url: browser.extension.getURL('pages/choice-screen.html'),
     });
