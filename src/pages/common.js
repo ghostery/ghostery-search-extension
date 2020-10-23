8ae4de2ad7a9b1bc7bb3aec9ca9ac34d20c85da7
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-10-23 16:26:04 +0200

    Choice Screen second step (#8)

--- src/pages/common.js
+++ src/pages/common.js
@@ -2,14 +2,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const engines = [...document.querySelectorAll('.engine')];
   engines.forEach(engine => {
     engine.addEventListener('click', async () => {
-      const name = engine.dataset.name;
-      await browser.ghostery.setDefaultSearchEngine(name);
-      const currentTab = await browser.tabs.getCurrent();
-      await browser.storage.local.set({
-        isOnboardingCompleted: true,
-      });
-      await browser.tabs.create({});
-      browser.tabs.remove(currentTab.id);
+      const name = engine.dataset.modal;
+      const modal = document.querySelector(`#${name}-modal`);
+      modal.classList.remove('hidden');
     });
   });
+
+  const cancelButtons = [...document.querySelectorAll('.modal .cancel-button')];
+  cancelButtons.forEach(button => {
+    button.addEventListener('click', closeModals);
+  });
+  const confirmButtons = [...document.querySelectorAll('.modal .confirm-button')];
+  confirmButtons.forEach(button => {
+    const name = button.dataset.name;
+    button.addEventListener('click', selectSearchEngine.bind(null, name));
+  });
 });
+
+async function selectSearchEngine(name) {
+  await browser.ghostery.setDefaultSearchEngine(name);
+  const currentTab = await browser.tabs.getCurrent();
+  await browser.storage.local.set({
+    isOnboardingCompleted: true,
+  });
+  await browser.tabs.create({});
+  browser.tabs.remove(currentTab.id);
+}
+
+function closeModals() {
+  const modals = [...document.querySelectorAll('.modal')];
+  modals.forEach(modal => {
+    modal.classList.add('hidden');
+  });
+}
