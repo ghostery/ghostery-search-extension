665c832058bff885465036788d5fd18932880a9f
Sam Macbeth <sam@cliqz.com>
2020-11-19 15:23:47 +0100

    Expose metrics to Ghostery extension via extension messaging.

---
+++ src/api.js
@@ -0,0 +1,14 @@
+/**
+ * API for external communication from Ghostery browser extension
+ */
+
+const ALLOWED_ADDON_IDS = new Set(["firefox@ghostery.com"]);
+
+browser.runtime.onMessageExternal.addListener(async (message, sender) => {
+  if (!ALLOWED_ADDON_IDS.has(sender.id)) {
+    return false;
+  }
+  if (message === "getMetrics") {
+    return getMetrics();
+  }
+});
