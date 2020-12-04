756d9d649e3021401026c52c055b1686892518e2
Sam Macbeth <sam@cliqz.com>
2020-12-04 10:11:28 +0100

    Unregister old content-script before registering the new one

--- src/status.js
+++ src/status.js
@@ -11,30 +11,37 @@ document.querySelector('html').classList.add('logged-out');
 `;
 
 function injectLoggedInStatus(status) {
-  statusContentScript = statusContentScript.then(async () => {
-    if (status === loggedIn) {
-      return;
-    }
-    loggedIn = status;
-    // register a content script to inject status classes in the future
-    await browser.contentScripts.register({
-      allFrames: false,
-      js: [
-        {
-          code: status ? loggedInScript : loggedOutScript,
-        },
-      ],
-      matches: [`${SERP_BASE_URL}/*`],
-      runAt: "document_start",
-    });
-    // trigger the log in state change in existing browser tabs
-    const activeTabs = await browser.tabs.query({ url: `${SERP_BASE_URL}/*` });
-    activeTabs.forEach(({ id }) => {
-      browser.tabs.executeScript(id, {
-        code: status ? loggedInScript : loggedOutScript,
+  statusContentScript = statusContentScript.then(
+    async (previousRegistration) => {
+      if (status === loggedIn) {
+        return;
+      }
+      // unregister previous content script
+      if (previousRegistration && previousRegistration.unregister) {
+        previousRegistration.unregister();
+      }
+      loggedIn = status;
+      // register a content script to inject status classes in the future
+      const registration = await browser.contentScripts.register({
+        allFrames: false,
+        js: [
+          {
+            code: status ? loggedInScript : loggedOutScript,
+          },
+        ],
+        matches: [`${SERP_BASE_URL}/*`],
+        runAt: "document_start",
       });
-    });
-  });
+      // trigger the log in state change in existing browser tabs
+      const activeTabs = await browser.tabs.query({
+        url: `${SERP_BASE_URL}/*`,
+      });
+      activeTabs.forEach(({ id }) => {
+        browser.tabs.executeScript(id, {
+          code: status ? loggedInScript : loggedOutScript,
+        });
+      });
+      return registration;
+    }
+  );
 }
-
-injectLoggedInStatus(false);
