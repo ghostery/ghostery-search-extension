3bd4a98fcf2f9cc28ff87c98f21f48559030d6b1
Krzysztof Jan Modras <chrmod@chrmod.net>
2021-02-02 16:09:25 +0100

    Remove Search Bar logic

--- src/experiment_apis/ghostery_parent.js
+++ src/experiment_apis/ghostery_parent.js
@@ -30,11 +30,6 @@ global.ghostery = class extends ExtensionCommon.ExtensionAPI {
 
           Services.search.defaultEngine = searchEngine;
         },
-        query(windowId, query) {
-          const window = getWindow(windowId);
-          window.gURLBar.value = query || '';
-          window.gURLBar.focus();
-        },
         isDefaultBrowser() {
           const shell = Components.classes["@mozilla.org/browser/shell-service;1"]
             .getService(Components.interfaces.nsIShellService)
